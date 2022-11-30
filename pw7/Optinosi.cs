688aa684ae9dea06e7b7e02dda24bb223f715938
RamzikMPT <112775544+RamzikMPT@users.noreply.github.com>
2022-11-30 13:34:21 +0300

    1com

---
+++ pw7/Optinosi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace pw7
+{
+    public class Optinosi
+    {
+        public static void Outputi()
+        {
+            DriveInfo c = DriveInfo.GetDrives()[0];
+            Console.WriteLine(c);
+        }
+    }
+}
